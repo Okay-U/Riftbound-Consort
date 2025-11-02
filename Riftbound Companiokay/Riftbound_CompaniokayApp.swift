f5c15229b5953b7c3e4b6f4f573752ea552961b1
okay <okay.uenal@icloud.com>
2025-11-02 12:12:39 +0100

    Initial Commit

---
+++ Riftbound Companiokay/Riftbound_CompaniokayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Riftbound_CompaniokayApp.swift
+//  Riftbound Companiokay
+//
+//  Created by Okay Kaan Ünal on 02.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Riftbound_CompaniokayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
